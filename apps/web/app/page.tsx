0c32267d48cfad0d19e74ebb204732ce94b24ff9
zeworks <josenogueira95@hotmail.com>
2024-03-26 02:03:33 +0000

    add initial landing page components

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,22 @@
+import { Header } from "./_components/header"
+
 export default function Home() {
 	return (
-		<main className="bg-background flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 max-w-5xl w-full items-center justify-between lg:flex">
-				<h1 className="bg-gradient-to-b from-white to-zinc-400 bg-clip-text font-extrabold tracking-[-0.04em] leading-none text-[40px] md:text-5xl lg:text-[80px] max-w-lg md:max-w-xl lg:max-w-4xl text-center text-transparent">
-					The build system that makes ship happen
-				</h1>
-			</div>
-		</main>
+		<>
+			<Header />
+			<main className="bg-background min-h-screen p-24">
+				<div className="z-10 max-w-4xl m-auto h-full w-full items-center justify-center flex flex-col text-center">
+					<h1 className="bg-gradient-to-b from-white to-zinc-400 bg-clip-text font-extrabold tracking-[-0.04em] leading-none text-[40px] md:text-5xl lg:text-[80px] text-center text-transparent">
+						Empowering teams while simplifying work
+					</h1>
+					<div className="max-w-2xl">
+						<p className="text-xl text-muted-foreground mt-5">
+							Experience seamless integration of efficiency and ease on our
+							platform, fostering optimal productivity for your team's success.
+						</p>
+					</div>
+				</div>
+			</main>
+		</>
 	)
 }
