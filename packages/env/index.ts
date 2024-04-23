109c75c4553974256f389c1295ddc538924b4822
zeworks <josenogueira95@hotmail.com>
2024-04-23 03:31:10 +0100

    update env typing

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -4,14 +4,18 @@ export const envSchema = z.object({
 	MODE: z.enum(["development", "production", "test"]).default("development"),
 	SERVER_PORT: z.string(),
 	WEB_PORT: z.string(),
-	JWT_SECRET: z.string(),
-	DATABASE_URL: z.string(),
+	JWT_SECRET: z.string().optional(),
+	DATABASE_URL: z.string().optional(),
 
 	// super admin configuration
-	SUPER_ADMIN_EMAIL: z.string(),
-	SUPER_ADMIN_PASSWORD: z.string(),
-	SUPER_ADMIN_NAME: z.string(),
+	SUPER_ADMIN_EMAIL: z.string().optional(),
+	SUPER_ADMIN_PASSWORD: z.string().optional(),
+	SUPER_ADMIN_NAME: z.string().optional(),
 	SUPER_ADMIN_AVATAR: z.string().optional(),
+
+	// Authentication Providers
+	GITHUB_CLIENT_ID: z.string().optional(),
+	GITHUB_CLIENT_SECRET: z.string().optional(),
 })
 
 const env = envSchema.parse(process.env)
