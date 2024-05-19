f2218d976643a9e3227029c205b4486be40d1ee6
zeworks <josenogueira95@hotmail.com>
2024-05-19 02:02:16 +0100

    Organizations | Fetch All Service
    - include fetch all service on organization module

--- packages/zod/schemas.ts
+++ packages/zod/schemas.ts
@@ -1,12 +1,36 @@
 import { z } from "zod"
 
 export const userStatus = z.enum(["ACTIVE", "INACTIVE", "PENDING", "BLOCKED"])
-export const userOrganizationRole = z.enum(["OWNER", "MEMBER", "USER"])
+export const memberRole = z.enum(["OWNER", "MEMBER", "USER"])
 
 export const base = z.object({
 	id: z.string().uuid(),
 	created_at: z.date().default(new Date()).nullable(),
 	updated_at: z.date().default(new Date()).optional().nullable(),
+})
+
+export const user = base.merge(
+	z.object({
+		id: z.string().uuid(),
+		name: z.string().min(4, "name must contain at least 4 characters"),
+		email: z.string().email({
+			message: "please enter a valid email",
+		}),
+		email_verified: z.date().optional().nullable(),
+		password: z
+			.string()
+			.min(8, "password too short (min 8 chars)")
+			.optional()
+			.nullable(),
+		status: userStatus.default("PENDING"),
+		avatar: z.string().optional().nullable(),
+	})
+)
+
+const member = z.object({
+	id: z.string().uuid(),
+	role: memberRole.default("USER"),
+	user,
 })
 
 export const organization = base.merge(
@@ -21,37 +45,22 @@ export const organization = base.merge(
 				message: "Name cannot be longer than 10 characters",
 			})
 			.describe("organization name"),
-		avatar: z.string().optional().describe("organization avatar"),
-	})
-)
-
-const userOrganization = z.object({
-	organization,
-	owner: z.boolean().default(false),
-	role: userOrganizationRole.default("USER"),
-})
-
-export const user = base.merge(
-	z.object({
-		id: z.string().uuid(),
-		name: z.string().min(4, "name must contain at least 4 characters"),
-		email: z.string().email({
-			message: "please enter a valid email",
-		}),
-		password: z
-			.string()
-			.min(8, "password too short (min 8 chars)")
+		avatar: z.string().optional().nullable().describe("organization avatar"),
+		owner: user,
+		members: z
+			.array(member)
 			.optional()
-			.nullable(),
-		status: userStatus.default("PENDING"),
-		avatar: z.string().optional().nullable(),
-		organizations: z.array(userOrganization).optional().nullable(),
+			.nullable()
+			.describe("organization members"),
 	})
 )
 
 export type Base = z.infer<typeof base>
-export type Organization = z.infer<typeof organization>
+
 export type User = z.infer<typeof user>
 export type UserStatus = z.infer<typeof userStatus>
-export type UserOrganizationRole = z.infer<typeof userOrganizationRole>
-export type UserOrganization = z.infer<typeof userOrganization>
+
+export type Member = z.infer<typeof member>
+export type MemberRole = z.infer<typeof memberRole>
+
+export type Organization = z.infer<typeof organization>
