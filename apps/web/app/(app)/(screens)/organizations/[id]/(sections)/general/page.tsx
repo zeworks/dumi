7434bbfe87c29b904c772ddfbdfa97a1ba554d4c
zeworks <josenogueira95@hotmail.com>
2024-10-10 01:04:45 +0100

    small updates

--- apps/web/app/(app)/(screens)/organizations/[id]/(sections)/general/page.tsx
+++ apps/web/app/(app)/(screens)/organizations/[id]/(sections)/general/page.tsx
@@ -1,13 +1,16 @@
 "use client"
+import { useGetOrganization } from "@/app/(app)/_services/organization"
 import { useParams } from "next/navigation"
 
 export default function Page(props: any) {
 	const params = useParams<{ id: string }>()
+	const org = useGetOrganization(params.id)
 
 	return (
 		<div className="flex flex-1 flex-col h-full">
 			<h1 className="mb-2 text-xl font-extrabold tracking-tight lg:text-2xl">
 				General Section
+				<pre className="text-sm">{JSON.stringify(org.data, null, 2)}</pre>
 			</h1>
 		</div>
 	)
---
+++ apps/web/app/utils/client.ts
@@ -0,0 +1,3 @@
+export const useClient = () => {
+	return typeof window !== "undefined"
+}
